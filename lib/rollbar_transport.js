redatvn/winston-rollbar

commit cd62259669e1dac3ffb699c253328ea4718d66a7

simplified, updated for winston@3

lib/rollbar_transport.js
@@ -2,93 +2,52 @@
 /**
  * Rollbar Transport
  *
- * A rollbar transport for winston.
+ * A rollbar transport for winston 3
+ * based on previous versions/forks:
+ * - https://github.com/golyakov/winston-rollbar
+ * - https://github.com/GorillaStack/winston-rollbar
+ * - https://github.com/Ideame/winston-rollbar
  */
 
-const winston = require('winston');
+const util = require('util');
+const TransportStream = require('winston-transport');
 const rollbar = require('rollbar');
-const cycle = require('cycle');
 
-
-exports.Rollbar = winston.transports.Rollbar = class extends winston.Transport {
-  constructor(opts = {}) {
+class RollbarTransport extends TransportStream {
+  constructor(opts) {
     super(opts);
     if (!opts.rollbarConfig.accessToken) {
       throw "winston-transport-rollbar requires a 'rollbarConfig.accessToken' property";
     }
-  
+
     const _rollbar = new rollbar(opts.rollbarConfig);
-  
-    this.name                 = 'rollbar';
-    this.level                = opts.level || 'warn';
-    this.metadataAsRequest    = opts.metadataAsRequest || false;
-    this.silent               = opts.silent || false;
-    this.rollbar              = _rollbar;
-    this.handleExceptions     = opts.handleExceptions || false;
+
+    this.name = 'rollbarnew';
+    this.level = opts.level || 'warn';
+    this.rollbar = _rollbar;
   }
 
-  /**
-   * Attempt to log to the given log level, otherwise log at the default level and include the level
-   * @param {String} level 
-   * @param {Array} args 
-   */
-  performLogging(level, ...args) {
-    const logMethod = this.rollbar[level];
-    if (logMethod) {
-      logMethod.apply(this.rollbar, args);
-    } else {
-      args[2].logLevel = level;
-      this.rollbar.log(...args);
-    }
+  log(info, callback) {
+    const {level, message} = info;
+    process.nextTick(() => {
+      const message = util.format(info.message, ...(info.splat || []));
+      const meta = info;
+      const rollbarLevel = level === 'warn' ? 'warning' : level;
+
+      const logMethod = this.rollbar[rollbarLevel] || this.rollbar.log;
+
+      return logMethod.apply(this.rollbar, [message, meta, cb]);
+
+      function cb(err) {
+        if (err) {
+          this.emit('error', err);
+          return callback(err);
+        }
+        this.emit('logged');
+        return callback(null, true);
+      }
+    });
   }
+}
 
-  /**
-  * Core logging method exposed to Winston. Metadata is optional.
-  * @method log
-  * @param level {string} Level at which to log the message
-  * @param msg {string} Message to log
-  * @param meta {Object} **Optional** Additional metadata to attach
-  * @param callback {function} Continuation to respond to when complete.
-  */
-  log(level, msg, meta, callback) {
-    const self = this;
-    if (this.silent) { return callback(null, true); }
-
-    let req = null;
-    if (typeof meta === 'string') { meta = { message: meta }; }
-    if (this.metadataAsRequest) {
-      if (req && req.socket) { req = meta; }
-    } else if (meta && meta.req) {
-      if (typeof meta.req === 'function') {
-        req = meta.req();
-      } else {
-        req = meta.req;
-      }
-    }
-
-    const cb = err => {
-      if (err) { return callback(err); }
-
-      self.emit('logged');
-      callback(null, true);
-    };
-
-    if (['warn', 'error'].indexOf(level) > -1 && (msg instanceof Error || meta instanceof Error)) {
-      var error;
-      meta.level = meta.level || level;
-      if (meta.level === 'warn') {
-        meta.level = 'warning';
-      }
-      if (msg instanceof Error) {
-        error = msg;
-      } else {
-        error = meta;
-      }
-      
-      this.performLogging(level, error, req, meta, cb);
-    } else {
-      const custom = typeof meta === 'object' ? cycle.decycle(meta) : meta;
-      this.performLogging(level, msg, req, custom, cb);
-    }
-  }
-};
+module.exports = RollbarTransport;

test/rollbar_transport_test.js
@@ -1,26 +1,0 @@
-const vows = require('vows');
-const assert = require('assert');
-const winston = require('winston');
-const helpers = require('winston/test/helpers');
-const Rollbar = require('../lib/rollbar_transport').Rollbar;
-
-
-function assertRollbar (transport) {
-    assert.instanceOf(transport, Rollbar);
-    assert.isFunction(transport.log);
-}
-
-var transport = new (winston.transports.Rollbar)({ rollbarConfig: { accessToken: '8802be7c990a4922beadaaefb6e0327b' } });
-
-vows.describe('rollbar_transport').addBatch({
-    "An instance of the Rollbar Transport": {
-        "should have the proper methods defined": function () {
-            assertRollbar(transport);
-        },
-        "the log() method": helpers.testNpmLevels(transport, "should log messages to Rollbar", function (ign, err, logged) {
-            if (err) throw err;
-            assert.isTrue(!err);
-            assert.isTrue(logged);
-        })
-    }
-}).export(module);

commit 66c67dc3e19670d45fac58eb754d8ff4d2b22576

Fix stream not being flushed
Currently this transport only sends the first log to Rollbar. All subsequent logs entirely bypass this transport. This happens because `callback` is never called because the `cb` is not bound to the current `this`.

This regression was introduced in cd6225966 when `cb` was converted from an arrow function (implicit binding) to a function expression (unbound).

lib/rollbar_transport.js
@@ -34,18 +34,17 @@ class RollbarTransport extends TransportStream {
       const meta = info;
       const rollbarLevel = level === 'warn' ? 'warning' : level;
 
-      const logMethod = this.rollbar[rollbarLevel] || this.rollbar.log;
-
-      return logMethod.apply(this.rollbar, [message, meta, cb]);
-
-      function cb(err) {
+      const cb = err => {
         if (err) {
           this.emit('error', err);
           return callback(err);
         }
         this.emit('logged');
         return callback(null, true);
-      }
+      };
+      
+      const logMethod = this.rollbar[rollbarLevel] || this.rollbar.log;
+      return logMethod.apply(this.rollbar, [message, meta, cb.bind(this)]);
     });
   }
 }